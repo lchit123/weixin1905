df8e3669b9a0ab1e0b49fd70bda13902e5fbb175
zhuyuan123 <1666919036@qq.com>
2019-12-09 17:42:02 +0800

    first

---
+++ app/Http/Controllers/Test/WeiXinController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WeiXinController extends Controller
+{
+//    public function admin(){
+//    	echo "123abc";
+//    }
+    public function  admin(Request $request)
+    {
+        echo 1;
+    }
+}
