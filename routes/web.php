313d19d9b668117ed1d5440af4ea63ceebcefa95
zhuyuan123 <1666919036@qq.com>
2019-12-10 10:59:40 +0800

    11

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test/','Test\WeiXinController@admin');
+// Route::get('/test','test\WeiXinController@admin');
+Route::get('one','test\WeixinControllerWadmin');
 
 
 
