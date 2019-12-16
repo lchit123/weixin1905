02243fe158b2b7214891dfc0889381dd60456a5e
zhu <zhu@localhost.localdomain>
2019-12-16 11:40:22 +0000

    提交

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/info',function(){
-	phpinfo();
-});
-Route::get('/test/hello','Test\TestController@hello');
-Route::get('/test/adduser','User\LoginController@addUser');
-Route::get('/test/redis1','Test\TestController@redis1');
-Route::get('/test/redis2','Test\TestController@redis2');
-Route::get('/test/xml','Test\TestController@xmlTest');
-Route::get('/test/baidu','Test\TestController@baidu');
-//微信开发
-Route::get('/wx','WeiXin\WxController@wechat');
-// Route::post('/wx','WeiXin\WxController@receiv');        //接收微信的推送事件
+Route::get('weixin/test','Wechat\WechatController@test');
+Route::get('weixin/index','Wechat\WechatController@checkSignature');//处理接入请求
+Route::post('weixin/index','Wechat\WechatController@receiv');         //接收微信的推送事件
+Route::get('weixin/media','Wechat\WechatController@getMedia');         //获取临时素材
+Route::get('/weixin/flush/access_token','Wechat\WechatController@flushAccessToken');        //刷新access_token
 
 
 
