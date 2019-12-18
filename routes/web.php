a537b78438eb04b00bdfae85152908b47efd4105
zhu <zhu@localhost.localdomain>
2019-12-18 05:35:50 +0000

    改

--- routes/web.php
+++ routes/web.php
@@ -22,23 +22,13 @@ Route::get('/weixin/flush/access_token','Wechat\WechatController@flushAccessToke
 */
 
 
-// Route::get('/wx/test','WeiXin\WxController@test');
-// Route::get('/wx','WeiXin\WxController@wechat');
-// Route::post('/wx','WeiXin\WxController@receiv');        //接收微信的推送事件
-// Route::get('/wx/media','WeiXin\WxController@getMedia');        //获取临时素材
-// Route::get('/wx/flush/access_token','WeiXin\WxController@flushAccessToken');        //刷新access_token
-// Route::get('/wx/menu','WeiXin\WxController@createMenu');        //创建菜单
-// Route::get('/wx/test','WeiXin\WxController@test');  
+Route::get('/wx/test','WeiXin\WxController@test');
+Route::get('/wx','WeiXin\WxController@wechat');
+Route::post('/wx','WeiXin\WxController@receiv');        //接收微信的推送事件
+Route::get('/wx/media','WeiXin\WxController@getMedia');        //获取临时素材
+Route::get('/wx/flush/access_token','WeiXin\WxController@flushAccessToken');        //刷新access_token
+Route::get('/wx/menu','WeiXin\WxController@createMenu');        //创建菜单
+Route::get('/wx/test','WeiXin\WxController@test');  
       //创建菜单
 
 
-Route::get('/wx/test','WeiXin\WxController@test');
-/*微信开发*/
-Route::get('/wx','WeiXin\WxController@wechat');
-/*接收用户的推送事件*/
-Route::post('/wx','WeiXin\WxController@receiv');
-/*获取临时素材*/
-Route::get('/wx/media','WeiXin\WxController@getMedia');
-/*自定义菜单栏*/
-Route::get('/wx/flush/access_token','WeiXin\WxController@flushAccessToken');
-Route::get('/wx/menu','WeiXin\WxController@createMenu');  //创建菜单
