884cc8dbad58e0a4938123647387de2e23a7ba26
zhu <zhu@localhost.localdomain>
2019-12-19 07:35:27 +0000

    测试

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -10,7 +10,23 @@ class WxController extends Controller
 { 
   public function wechat()
   { 
-
+    $token='abc123token';//设置好的token
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce = $_GET["nonce"];
+    $echostr=$_GET["echostr"];
+    
+     
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+    
+    if( $tmpStr == $signature ){   //验证通过
+        echo $echostr;
+    }else{
+        die("not ok");
+    }
 
 
   }
