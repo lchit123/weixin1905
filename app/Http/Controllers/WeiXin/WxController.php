f6b7949f09bdabe2fe89edccc375e902a985f6b7
zhu <zhu@localhost.localdomain>
2019-12-20 02:59:11 +0000

    时间

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -36,7 +36,8 @@ class WxController extends Controller
   { 
     $log_file="wx.log";
     //将接收的数据记录到日志文件中
-    $data=json_encode($_POST);
+    $xml=file_get_contents("php://input");
+    $data=date('Y-m-d H:i:s').$xml;
     file_put_contents($log_file,$data,FILE_APPEND);   //追加写日志
   }
 
