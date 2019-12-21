9fc9680d00f8285cc3d072ae3ba313a820977b56
zhu <zhu@localhost.localdomain>
2019-12-21 13:14:38 +0000

    ss

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -8,6 +8,21 @@ use GuzzleHttp\Client;
  
 class WxController extends Controller
 { 
+
+  public function __construct()
+  { 
+    //获取access_token
+    $this->access_token=$this->getAccessToken();
+
+  }
+
+  protected function getAccessToken()
+  { 
+
+    $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=. ).&secret=env(' 
+
+  }
+
  //处理接入
   public function wechat()
   { 
@@ -36,11 +51,19 @@ class WxController extends Controller
   { 
     $log_file="wx.log";
     //将接收的数据记录到日志文件中
-    $xml_str=file_put_contents("php://input");
+    $xml_str=file_get_contents("php://input");
     $data=date('Y-m-d H:i:s').$xml;
     file_put_contents($log_file,$data,FILE_APPEND);   //追加写日志
 
+    //处理xml数据
     $xml_arr=simplexml_load_string($xml_str);
+    $event=$xml_obj->Event;   //获取事件类型
+    $openid=$xml_obj->FromUserName;  //获取用户的openid
+    if($event=='subscribe'){ 
+      //获取用户信息
+      $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+
+    }
   }
 
 
@@ -50,6 +73,10 @@ class WxController extends Controller
   { 
 
     $url='https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN';
+    //发送网络请求
+    $json_str=file_get_contents($url);
+    $log_file='wx_user.log';
+    file_put_contents($log_file,$json_str,FILE_APPEND);
 
   }
 
