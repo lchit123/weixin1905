c14ceea0f50a22c2e8aac94dbe3b84aa2e7841dc
zhu <zhu@localhost.localdomain>
2019-12-21 05:40:30 +0000

    pos

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/weixin/flush/access_token','Wechat\WechatController@flushAccessToke
 // Route::get('/wx/menu','WeiXin\WxController@createMenu');        //创建菜单
 // Route::get('/wx/test','WeiXin\WxController@test');  
  ;
-Route::get('/test/xml','Test\TestController@xmlTest');
+Route::post('/test/xml','Test\TestController@xmlTest');
  
  
 
