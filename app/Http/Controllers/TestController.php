ccd28d8cf6e6b0543952c601eff7af530ad0ce2c
zhu <zhu@qq.com>
2020-01-02 05:36:28 +0000

    1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -23,7 +23,7 @@ class TestController extends Controller
         // 请求参数
         $out_trade_no = time() . rand(1111,9999);       //商户订单号
         $product_code = 'FAST_INSTANT_TRADE_PAY';
-        $total_amount = 10000;
+        $total_amount = 99.999999;
         $subject = '测试订单' . $out_trade_no;
         $request_param = [
             'out_trade_no'  => $out_trade_no,
